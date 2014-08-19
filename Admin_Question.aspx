b0d1a434f533be6deb23253d806b6dedda282b7d
IgorSudijovski <IgorSudijovski@3f973c04-e464-07b5-3deb-0c95e1258471>
2014-08-19 21:30:16 +0000

    added admin page for questions

---
+++ Admin_Question.aspx
@@ -0,0 +1,106 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="Admin_Question.aspx.cs" Inherits="_Default" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="navigationBar" ContentPlaceHolderID="mainLinks" Runat="Server">
+    
+    <asp:HyperLink ID="users" runat="server" CssClass="main-links" 
+        NavigateUrl="~/Admin_Users.aspx">Корисници</asp:HyperLink>
+    <asp:HyperLink ID="associations" runat="server" CssClass="main-links" 
+        NavigateUrl="~/Admin_Association.aspx">Асоцијација</asp:HyperLink>
+        <asp:HyperLink ID="lnkQuestions" runat="server" CssClass="main-links text-bold" 
+        NavigateUrl="~/Admin_Question.aspx">Прашања</asp:HyperLink>
+        <div class="logedUser"> Вие сте најавен како <asp:Label ID="lblLogedUser" 
+                runat="server" Text="Label" Font-Bold="True"></asp:Label></div>
+    <asp:LinkButton ID="logOutButton" runat="server" 
+        CssClass="main-links log-out-button" onclick="logOut_click">Одлогирај се</asp:LinkButton>
+        
+</asp:Content>
+<asp:Content ID="mainContent" ContentPlaceHolderID="mainContent" Runat="Server">
+    <div class="main-content">
+    <div>
+        <asp:Label ID="lstBoxLabel" runat="server" Text="Опис на прашањето" 
+            Width="200px" CssClass="vertical"></asp:Label>
+        <asp:ListBox ID="lstBoxQuestions" runat="server" Height="100px" Width="300px" 
+            AutoPostBack="True" onselectedindexchanged="selectQuestionForEditing"></asp:ListBox>
+    </div>
+    <div style="margin-bottom: 15px">
+        <asp:Label ID="error" runat="server" ForeColor="Red" Visible="False"></asp:Label>
+    </div>
+    <div>
+        <asp:Label ID="lblUserName" runat="server" Text="Опис на прашањето" Width="200px"></asp:Label>
+        <asp:TextBox ID="txtOpis" runat="server" CssClass="margin-right" 
+            ValidationGroup="validation1" Width="200px"></asp:TextBox>
+        <asp:RegularExpressionValidator ID="RegularExpressionValidator3" runat="server" 
+            ControlToValidate="txtOpis" CssClass="margin-right" 
+            ErrorMessage="Може да содржи мали букви, големи букви и бројки" 
+            ForeColor="Red" ValidationExpression="^[a-zA-Z0-9]*$" 
+            ValidationGroup="validation1"></asp:RegularExpressionValidator>
+        <asp:RegularExpressionValidator ID="RegularExpressionValidator4" runat="server"     
+            ControlToValidate="txtOpis" ErrorMessage="Минмална големина од 4 карактери" 
+            ForeColor="Red" ValidationExpression=".{4,}" CssClass="margin-right" 
+            ValidationGroup="validation1"></asp:RegularExpressionValidator>
+        <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" 
+            ControlToValidate="txtOpis" ForeColor="Red" ValidationGroup="validation1">Задолжително поле</asp:RequiredFieldValidator>
+    </div>
+    <div>
+        <asp:Label ID="lblUserName0" runat="server" Text="Прашање" Width="200px" 
+            CssClass="vertical"></asp:Label>
+        <asp:TextBox ID="txtQuestion" runat="server" CssClass="margin-right" Rows="3" 
+            TextMode="MultiLine" Width="300px"></asp:TextBox>
+        <asp:RequiredFieldValidator ID="RequiredFieldValidator2" runat="server" 
+            ControlToValidate="txtQuestion" ForeColor="Red" 
+            ValidationGroup="validation1">Задолжително поле</asp:RequiredFieldValidator>
+    </div>
+    <div>
+        <asp:Label ID="lblAnswer1" runat="server" Text="Одговор 1" Width="200px"></asp:Label>
+        <asp:TextBox ID="txtAnswer1" runat="server" CssClass="margin-right" 
+            ValidationGroup="validation1" Width="200px"></asp:TextBox>
+        <asp:RadioButton ID="RadioButtonAnswer1" runat="server" Checked="True" 
+            CssClass="margin-right" GroupName="Answer" Text="Точен одговор" />
+        <asp:RequiredFieldValidator ID="RequiredFieldValidator3" runat="server" 
+            ControlToValidate="txtAnswer1" ForeColor="Red" 
+            ValidationGroup="validation1">Задолжително поле</asp:RequiredFieldValidator>
+        </div>
+    <div>
+    <asp:Label ID="lblAnswer2" runat="server" Text="Одговор 2" Width="200px"></asp:Label>
+        <asp:TextBox ID="txtAnswer2" runat="server" CssClass="margin-right" 
+            ValidationGroup="validation1" Width="200px"></asp:TextBox>
+        <asp:RadioButton ID="RadioButtonAnswer2" runat="server" Checked="false" 
+            CssClass="margin-right" GroupName="Answer" Text="Точен одговор" />
+        <asp:RequiredFieldValidator ID="RequiredFieldValidator4" runat="server" 
+            ControlToValidate="txtAnswer2" ForeColor="Red" 
+            ValidationGroup="validation1">Задолжително поле</asp:RequiredFieldValidator>
+            </div>
+    <div>
+    <asp:Label ID="lblAnswer3" runat="server" Text="Одговор 3" Width="200px"></asp:Label>
+        <asp:TextBox ID="txtAnswer3" runat="server" CssClass="margin-right" 
+            ValidationGroup="validation1" Width="200px"></asp:TextBox>
+        <asp:RadioButton ID="RadioButtonAnswer3" runat="server" Checked="false" 
+            CssClass="margin-right" GroupName="Answer" Text="Точен одговор" />
+        <asp:RequiredFieldValidator ID="RequiredFieldValidator5" runat="server" 
+            ControlToValidate="txtAnswer3" ForeColor="Red" 
+            ValidationGroup="validation1">Задолжително поле</asp:RequiredFieldValidator></div>
+    <div>
+    <asp:Label ID="lblAnswer4" runat="server" Text="Одговор 4" Width="200px"></asp:Label>
+        <asp:TextBox ID="txtAnswer4" runat="server" CssClass="margin-right" 
+            ValidationGroup="validation1" Width="200px"></asp:TextBox>
+        <asp:RadioButton ID="RadioButtonAnswer4" runat="server" Checked="false" 
+            CssClass="margin-right" GroupName="Answer" Text="Точен одговор" />
+        <asp:RequiredFieldValidator ID="RequiredFieldValidator6" runat="server" 
+            ControlToValidate="txtAnswer4" ForeColor="Red" 
+            ValidationGroup="validation1">Задолжително поле</asp:RequiredFieldValidator></div>
+<div>
+    <asp:Button ID="btnAdd" runat="server" CssClass="margin-right" 
+        Text="Додади" onclick="addQuestionClick" ValidationGroup="validation1" />
+    <asp:Button ID="btnSave" runat="server" CssClass="margin-right" 
+        Text="Зачувај" Enabled="False" onclick="saveQuestionClick" 
+        ValidationGroup="validation1" />
+    <asp:Button ID="btnCancel" runat="server" CssClass="margin-right" Text="Откажи" 
+        Enabled="False" onclick="cancelClick" />
+    <asp:Button ID="btnDelete" runat="server" CssClass="margin-right" 
+        Text="Избриши" Enabled="False" onclick="deleteClick" />
+</div>
+</div>
+</asp:Content>
+
---
+++ Admin_Question.aspx.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        error.Visible = false;
+        if (!this.IsPostBack)
+        {
+            String username = (String)Session["username"];
+            if (username == null)
+            {
+                Response.Redirect("~/Login.aspx");
+            }
+            int role = (int)Session["username_role"];
+            if (role != 1)
+            {
+                Response.Redirect("~/Login.aspx");
+            }
+            lblLogedUser.Text = username;
+            loadQuestions();
+        }
+    }
+    protected void selectQuestionForEditing(object sender, EventArgs e)
+    {
+        String name = lstBoxQuestions.SelectedValue;
+        btnAdd.Enabled = false;
+        btnCancel.Enabled = true;
+        btnDelete.Enabled = true;
+        btnSave.Enabled = true;
+        txtOpis.Enabled = false;
+        SqlConnection connection = new SqlConnection();
+        String connectionString = ConfigurationManager.ConnectionStrings["dataBaseConnection"].ConnectionString;
+        connection.ConnectionString = connectionString;
+        SqlCommand command = new SqlCommand();
+        command.Connection = connection;
+        command.CommandText = "SELECT * FROM questions WHERE name=@name";
+        command.Parameters.AddWithValue("@name", name);
+        try
+        {
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            reader.Read();
+            txtOpis.Text = (String)reader["name"];
+            txtQuestion.Text = (String)reader["question"];
+            txtAnswer1.Text = (String)reader["answer1"];
+            txtAnswer2.Text = (String)reader["answer2"];
+            txtAnswer3.Text = (String)reader["answer3"];
+            txtAnswer4.Text = (String)reader["answer4"];
+            selectRadioButton((String)reader["realanswer"]);
+            reader.Close();
+        }
+        catch (Exception ex)
+        {
+            error.Text = ex.Message;
+            error.Visible = true;
+        }
+        finally
+        {
+            connection.Close();
+        }
+    }
+    protected void addQuestionClick(object sender, EventArgs e)
+    {
+        error.Visible = false;
+        SqlConnection connection = new SqlConnection();
+        String connectionString = ConfigurationManager.ConnectionStrings["dataBaseConnection"].ConnectionString;
+        connection.ConnectionString = connectionString;
+        SqlCommand command = new SqlCommand();
+        command.Connection = connection;
+        command.CommandText = "SELECT name FROM questions WHERE name=@name";
+        command.Parameters.AddWithValue("@name", txtOpis.Text);
+        try
+        {
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            if (reader.HasRows)
+            {
+                error.Text = "Веќе постои таков опис на прашање";
+                error.Visible = true;
+                reader.Close();
+            }
+            else
+            {
+                reader.Close();
+                command.Parameters.Clear();
+                command.CommandText = "INSERT INTO questions(name, question, answer1, answer2, answer3, answer4, realanswer) VALUES (@name, @question, @answer1, @answer2, @answer3, @answer4, @realanswer)";
+                command.Parameters.AddWithValue("@name", txtOpis.Text);
+                command.Parameters.AddWithValue("@question", txtQuestion.Text);
+                command.Parameters.AddWithValue("@answer1", txtAnswer1.Text);
+                command.Parameters.AddWithValue("@answer2", txtAnswer2.Text);
+                command.Parameters.AddWithValue("@answer3", txtAnswer3.Text);
+                command.Parameters.AddWithValue("@answer4", txtAnswer4.Text);
+                command.Parameters.AddWithValue("@realanswer", getRealAnswer());
+                command.ExecuteNonQuery();
+                initAllRadioButtons();
+                txtOpis.Text = "";
+                txtQuestion.Text = "";
+                txtAnswer1.Text = "";
+                txtAnswer2.Text = "";
+                txtAnswer3.Text = "";
+                txtAnswer4.Text = "";
+                txtOpis.Enabled = true;
+                lstBoxQuestions.SelectedIndex = -1;
+                loadQuestions();
+            }
+        }
+        catch (Exception ex)
+        {
+            error.Text = ex.Message;
+            error.Visible = true;
+        }
+        finally
+        {
+            connection.Close();
+        }
+    }
+    protected void saveQuestionClick(object sender, EventArgs e)
+    {
+        btnAdd.Enabled = true;
+        btnCancel.Enabled = false;
+        btnDelete.Enabled = false;
+        btnSave.Enabled = false;
+        SqlConnection connection = new SqlConnection();
+        String connectionString = ConfigurationManager.ConnectionStrings["dataBaseConnection"].ConnectionString;
+        connection.ConnectionString = connectionString;
+        SqlCommand command = new SqlCommand();
+        command.Connection = connection;
+        command.CommandText = "UPDATE questions SET question=@question, answer1=@answer1, answer2=@answer2, answer3=@answer3, answer4=@answer4, realanswer=@realanswer WHERE name=@name";
+        command.Parameters.AddWithValue("@name", txtOpis.Text);
+        command.Parameters.AddWithValue("@question", txtQuestion.Text);
+        command.Parameters.AddWithValue("@answer1", txtAnswer1.Text);
+        command.Parameters.AddWithValue("@answer2", txtAnswer2.Text);
+        command.Parameters.AddWithValue("@answer3", txtAnswer3.Text);
+        command.Parameters.AddWithValue("@answer4", txtAnswer4.Text);
+        command.Parameters.AddWithValue("@realanswer", getRealAnswer());
+        try
+        {
+            connection.Open();
+            command.ExecuteNonQuery();
+        }
+        catch (Exception ex)
+        {
+            error.Text = ex.Message;
+            error.Visible = true;
+        }
+        finally
+        {
+            connection.Close();
+        }
+        initAllRadioButtons();
+        txtOpis.Text = "";
+        txtQuestion.Text = "";
+        txtAnswer1.Text = "";
+        txtAnswer2.Text = "";
+        txtAnswer3.Text = "";
+        txtAnswer4.Text = "";
+        txtOpis.Enabled = true;
+        lstBoxQuestions.SelectedIndex = -1;
+    }
+    protected void cancelClick(object sender, EventArgs e)
+    {
+        btnAdd.Enabled = true;
+        btnCancel.Enabled = false;
+        btnDelete.Enabled = false;
+        btnSave.Enabled = false;
+        initAllRadioButtons();
+        txtOpis.Text = "";
+        txtQuestion.Text = "";
+        txtAnswer1.Text = "";
+        txtAnswer2.Text = "";
+        txtAnswer3.Text = "";
+        txtAnswer4.Text = "";
+        txtOpis.Enabled = true;
+        lstBoxQuestions.SelectedIndex = -1;
+    }
+    protected void deleteClick(object sender, EventArgs e)
+    {
+        btnAdd.Enabled = true;
+        btnCancel.Enabled = false;
+        btnDelete.Enabled = false;
+        btnSave.Enabled = false;
+        SqlConnection connection = new SqlConnection();
+        String connectionString = ConfigurationManager.ConnectionStrings["dataBaseConnection"].ConnectionString;
+        connection.ConnectionString = connectionString;
+        SqlCommand command = new SqlCommand();
+        command.Connection = connection;
+        command.CommandText = "DELETE FROM questions WHERE name=@name";
+        command.Parameters.AddWithValue("@name", txtOpis.Text);
+        try
+        {
+            connection.Open();
+            command.ExecuteNonQuery();
+        }
+        catch (Exception ex)
+        {
+            error.Text = ex.Message;
+            error.Visible = true;
+        }
+        finally
+        {
+            connection.Close();
+        }
+        initAllRadioButtons();
+        txtOpis.Text = "";
+        txtQuestion.Text = "";
+        txtAnswer1.Text = "";
+        txtAnswer2.Text = "";
+        txtAnswer3.Text = "";
+        txtAnswer4.Text = "";
+        txtOpis.Enabled = true;
+        lstBoxQuestions.SelectedIndex = -1;
+        loadQuestions();
+    }
+    protected void logOut_click(object sender, EventArgs e)
+    {
+        Session.Remove("username");
+        Session.Remove("username_role");
+        HttpCookie keepMeLoginCookie = Request.Cookies["username"];
+        if (keepMeLoginCookie != null)
+        {
+            keepMeLoginCookie.Expires = DateTime.Now.AddDays(-1);
+            Response.Cookies.Add(keepMeLoginCookie);
+        }
+        Response.Redirect("~/Login.aspx");
+    }
+
+    private void loadQuestions()
+    {
+        lstBoxQuestions.Items.Clear();
+        SqlConnection connection = new SqlConnection();
+        String connectionString = ConfigurationManager.ConnectionStrings["dataBaseConnection"].ConnectionString;
+        connection.ConnectionString = connectionString;
+        SqlCommand command = new SqlCommand();
+        command.Connection = connection;
+        command.CommandText = "SELECT name FROM questions";
+        try
+        {
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                ListItem lstItem = new ListItem();
+                lstItem.Text = (String)reader["name"];
+                lstItem.Value = (String)reader["name"];
+                lstBoxQuestions.Items.Add(lstItem);
+            }
+            reader.Close();
+        }
+        catch (Exception ex)
+        {
+            error.Text = ex.Message;
+            error.Visible = true;
+        }
+        finally
+        {
+            connection.Close();
+        }
+    }
+
+    private void selectRadioButton(String answer)
+    {
+        resetAllRadioButtons();
+        if (answer == txtAnswer1.Text)
+        {
+            RadioButtonAnswer1.Checked = true;
+        }
+        if (answer == txtAnswer2.Text)
+        {
+            RadioButtonAnswer2.Checked = true;
+        }
+        if (answer == txtAnswer3.Text)
+        {
+            RadioButtonAnswer3.Checked = true;
+        }
+        if (answer == txtAnswer4.Text)
+        {
+            RadioButtonAnswer4.Checked = true;
+        }
+    }
+
+    private String getRealAnswer()
+    {
+        if (RadioButtonAnswer1.Checked)
+        {
+            return txtAnswer1.Text;
+        }
+        if (RadioButtonAnswer2.Checked)
+        {
+            return txtAnswer2.Text;
+        }
+        if (RadioButtonAnswer3.Checked)
+        {
+            return txtAnswer3.Text;
+        }
+        return txtAnswer4.Text;
+    }
+
+    private void resetAllRadioButtons()
+    {
+        RadioButtonAnswer1.Checked = false;
+        RadioButtonAnswer2.Checked = false;
+        RadioButtonAnswer3.Checked = false;
+        RadioButtonAnswer4.Checked = false;
+    }
+
+    private void initAllRadioButtons()
+    {
+        RadioButtonAnswer1.Checked = true;
+        RadioButtonAnswer2.Checked = false;
+        RadioButtonAnswer3.Checked = false;
+        RadioButtonAnswer4.Checked = false;
+    }
+}
